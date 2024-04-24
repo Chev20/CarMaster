18dfb1cee44470f3d2a7768b095f604e142d3f09
Chev20 <chelioschev2020@gmail.com>
2024-04-24 18:05:36 +0300

    add librariesadding libraries via composer

--- CarMaster/Auto.php
+++ CarMaster/Auto.php
@@ -31,12 +31,6 @@ class Auto
         return $this->brand;
     }
 
-<<<<<<< HEAD
-=======
-    /**
-     * @return string
-     */
->>>>>>> 3f5e52c2be85b03460f5817391194ec9b4fc3c39
     public function getModel(): string
     {
         return $this->model;
@@ -124,9 +118,6 @@ class Auto
             'Mileage' => $this->getCarMileage(),
         ];
     }
-<<<<<<< HEAD
 }
-=======
 
-}
->>>>>>> 3f5e52c2be85b03460f5817391194ec9b4fc3c39
+
